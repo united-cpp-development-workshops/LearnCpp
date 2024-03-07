c8a15cff16c9dff009ba18df2e5c4721862a58b6
EmrecanKaracayir <developer@karacayir.com>
2024-03-07 22:11:58 +0300

    added: Workshop02

--- Chapter00/src/announce.hpp
+++ Chapter00/src/announce.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifndef ANNOUNCE_CHAPTER
-# define ANNOUNCE_CHAPTER(msg) std::cout << msg << "\n" << std::string(std::strlen(msg), '=') << "\n\n"
+# define ANNOUNCE_CHAPTER(msg) std::cout << msg << "\n" << std::string(std::strlen(msg), '=') << "\n"
 #endif
 
 #ifndef ANNOUNCE_WORKSHOP
-# define ANNOUNCE_WORKSHOP(msg) std::cout << msg << "\n" << std::string(std::strlen(msg), '-') << "\n"
+# define ANNOUNCE_WORKSHOP(msg) std::cout << "\n" << msg << "\n" << std::string(std::strlen(msg), '-') << "\n"
 #endif
--- Chapter00/src/main.cpp
+++ Chapter00/src/main.cpp
@@ -2,10 +2,12 @@
 
 #include "announce.hpp"
 #include "workshops/workshop01.hpp"
+#include "workshops/workshop02.hpp"
 
 int main()
 {
   ANNOUNCE_CHAPTER("Chapter 00: Introduction / Getting Started");
   Workshop01::run();
+  Workshop02::run();
   return 0;
 }
